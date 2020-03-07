22f201c87bd50273c5baa9583a6d96021b8e0cf3
Mohit Gupta <mohit.gupta@rentomojo.com>
2020-03-07 19:11:24 +0530

    Enhancement: Don't stop consuming next commands if prev. command if malformed.

--- input_consumers/file_input_consumer.py
+++ input_consumers/file_input_consumer.py
@@ -3,9 +3,9 @@ class FileInputConsumer:
         self.processor = processor
 
     def consume(self, input_file):
-        try:
             with open(input_file) as file:
                 for command in file:
-                    print(self.processor.process(command[:-1]))
-        except Exception as e:
-            print(e.msg)
+                    try:
+                        print(self.processor.process(command[:-1]))
+                    except Exception as e:
+                        print(e.msg)
